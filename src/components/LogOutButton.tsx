18d1eff739f10a45d607e3cd7a93630e261c5506
Arun9650 <arun02580@gmail.com>
2024-09-28 18:22:44 +0530

    Refactor LogOutButton component to import 'use server' and remove unused code

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,25 +1,19 @@
-'use server'
+
 import { signOut } from 'next-auth/react';
 import React from 'react'
-import { cookies } from 'next/headers'
 
 
 const LogOutButton = () => {
     const handleLogout =  () => {
+        'use server'
+        const { cookies } = await import('header'); // Dynamically import cookies from header
         const cookieStore = cookies()
         // Sign out and redirect to the sign-in page
         signOut({
           callbackUrl: '/auth/signin',
         });
-          // Manually clear session-related cookies if necessary
-          document.cookie = '__Secure-next-auth.session-token=; Max-Age=0; path=/; secure;';
-          document.cookie = 'next-auth.session-token=; Max-Age=0; path=/;';
-          document.cookie = 'next-auth.csrf-token=; Max-Age=0; path=/;';
-        
           
           cookieStore.delete('__Secure-next-auth.session-token');
-        window.localStorage.clear();
-        window.sessionStorage.clear();
         
       };
   return (
