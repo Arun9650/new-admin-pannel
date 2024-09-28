c69b588f39f77101db132ac0a4c7b399554f3993
Arun9650 <arun02580@gmail.com>
2024-09-28 17:54:41 +0530

    Refactor sign-out button to remove unused code, handle logout asynchronously, and clear session-related cookies

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -26,7 +26,15 @@ const handleLogout =  () => {
   // Sign out and redirect to the sign-in page
   signOut({
     callbackUrl: '/auth/signin',
-  })
+  });
+    // Manually clear session-related cookies if necessary
+    document.cookie = '__Secure-next-auth.session-token=; Max-Age=0; path=/; secure;';
+    document.cookie = 'next-auth.session-token=; Max-Age=0; path=/;';
+    document.cookie = 'next-auth.csrf-token=; Max-Age=0; path=/;';
+  
+    
+  window.localStorage.clear();
+  window.sessionStorage.clear();
 };
 
   return (
